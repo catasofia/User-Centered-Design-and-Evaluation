bd2b54e8927e62d7f3be2049020a2f42fa988969
Leonor Morgado <leonor.morgado@tecnico.ulisboa.pt>
2022-01-04 10:46:16 +0000

    Novas pastas para dividir o código do tenant, neighbor e landlord na lib

--- lib/tenant/main.dart
+++ lib/tenant/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'loading.dart';
-import 'tenantHomescreen.dart';
-import 'tenantTasks.dart';
-import 'tenantContacts.dart';
+import '../loading.dart';
+import '../tenant/tenantHomescreen.dart';
+import '../tenant/tenantTasks.dart';
+import '../tenant/tenantContacts.dart';
 
 void main() => runApp(MaterialApp(
     initialRoute: '/',
