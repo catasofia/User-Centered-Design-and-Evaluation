b0fe134e6d0b30b57e209868dc3c586645417c5c
Leonor Morgado <leonor.morgado@tecnico.ulisboa.pt>
2022-01-06 20:59:54 +0000

    Chat for the tenant working

---
+++ lib/tenant/tenantChat2.dart
@@ -0,0 +1,400 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'tenantTasks.dart';
+import 'tenantHomescreen.dart';
+
+class Chat2 extends StatefulWidget {
+  String description;
+  Chat2({Key? key, required this.description}) : super(key: key);
+
+  @override
+  _Chat2State createState() => _Chat2State();
+}
+
+class _Chat2State extends State<Chat2> {
+
+  final text = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    text.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String _description = widget.description;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomAppBar(
+        color: Color(0xFF48ACBE),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.house_outlined,
+                  size: 35.0,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Home()),
+                  );
+                },
+              ),
+              IconButton(icon: Icon(
+                Icons.star_border,
+                color: Colors.white,
+                size: 35.0,
+              ), onPressed: () {}),
+              IconButton(
+                  icon: Icon(
+                    Icons.cleaning_services_rounded,
+                    size: 30.0,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Task()),
+                    );
+                  }
+              ),
+              IconButton(
+                  color: Colors.black,
+                  icon: Icon(
+                    Icons.chat_bubble_outline_rounded,
+                    size: 30.0,
+                  ), onPressed: () {}),
+            ],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.person_outline,
+                  color: Colors.black,
+                  size: 40.0,
+                ),
+                SizedBox(width: 15.0),
+                Container(
+                  width: 240.0,
+                  height: 42.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24.0),
+                    color: Colors.grey[200],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(width: 15.0),
+                      Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        'Search',
+                        style: TextStyle(
+                          fontFamily: 'Arial',
+                          fontSize: 20,
+                          color: Colors.black,
+                          height: 1,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 8.0),
+                IconButton(
+                  icon: const Icon(
+                    Icons.notifications_active_outlined,
+                    color: Colors.black,
+                    size: 38.0,
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) => _buildPopupNotification(context),
+                    );
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: [
+                    Container(
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/carlos.jfif'),
+                        radius: 40.0,
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      'Carlos',
+                      style: TextStyle(
+                        fontFamily: 'Arial',
+                        fontSize: 18,
+                        color: Colors.black,
+                        height: 1,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'December 10th',
+              style: TextStyle(
+                fontFamily: 'Arial',
+                fontSize: 22,
+                color: Colors.black,
+                height: 1,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  width: 220.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1.5,
+                        blurRadius: 1.5,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                    color: Color(0xFF9ED3DD),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(7.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Text(
+                          'Of course!',
+                          style: TextStyle(
+                            fontFamily: 'Arial',
+                            fontSize: 20,
+                            color: Colors.black,
+                            height: 1,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15.0),
+            Text(
+              'Today',
+              style: TextStyle(
+                fontFamily: 'Arial',
+                fontSize: 22,
+                color: Colors.black,
+                height: 1,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Container(
+              width: 220.0,
+              height: 80.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1.5,
+                    blurRadius: 1.5,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+                color: Color(0xFF48ACBE),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(7.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Good morning, have you switched the lightbulb already?',
+                      style: TextStyle(
+                        fontFamily: 'Arial',
+                        fontSize: 20,
+                        color: Colors.white,
+                        height: 1,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  width: 220.0,
+                  height: 80.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1.5,
+                        blurRadius: 1.5,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                    color: Color(0xFF9ED3DD),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(7.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Text(
+                          '$_description',
+                          style: TextStyle(
+                            fontFamily: 'Arial',
+                            fontSize: 20,
+                            color: Colors.black,
+                            height: 1,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15.0),
+            Row(
+              children: [
+                Container(
+                  width: 300,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Chat',
+                    ),
+                    controller: text,
+                  ),
+                ),
+                SizedBox(width: 3.0),
+                IconButton(
+                  icon: Icon(
+                    Icons.send_rounded,
+                    size: 30.0,
+                    color: Color(0xFF48ACBE),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home()),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+Widget _buildPopupNotification(BuildContext context) {
+  return new AlertDialog(
+    alignment: Alignment.center,
+    title: const Text(
+      'Notifications',
+      style: TextStyle(
+        fontFamily: 'Arial',
+        fontSize: 30,
+        color: Colors.white,
+        height: 1,
+      ),
+      textAlign: TextAlign.center,
+    ),
+    backgroundColor: Color(0xFF48ACBE),
+    content: new Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "- João completed a task, rate him now.",
+          style: TextStyle(
+            fontFamily: 'Arial',
+            fontSize: 20,
+            color: Colors.black,
+            height: 1,
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Text(
+          "- You have two days to complete your task.",
+          style: TextStyle(
+            fontFamily: 'Arial',
+            fontSize: 20,
+            color: Colors.black,
+            height: 1,
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Text(
+          "- Carlos added a new task.",
+          style: TextStyle(
+            fontFamily: 'Arial',
+            fontSize: 20,
+            color: Colors.black,
+            height: 1,
+          ),
+        ),
+      ],
+    ),
+    actions: <Widget>[
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          new FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            textColor: Theme.of(context).primaryColor,
+            child: const Icon(
+              Icons.remove_circle_outline,
+              color: Colors.black,
+              size: 25.0,
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+}
