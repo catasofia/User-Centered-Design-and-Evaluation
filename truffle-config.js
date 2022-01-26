6440221509169d80cf6a8b851e8b96f54886f174
catasofia <47949684+catasofia@users.noreply.github.com>
2022-01-26 11:07:26 +0000

    blockchain warning and blockchain stuff

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,18 @@
+
 module.exports = {
   networks: {
      development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
+      port: 8545,   // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
      },
   },
     contracts_build_directory: "./src/artifacts/",
-      
+
   // Configure your compilers
   compilers: {
-    solc: {    
-      
+    solc: {
+        version: "^0.4.18",
        // See the solidity docs for advice
        // about optimization and evmVersion
         optimizer: {
