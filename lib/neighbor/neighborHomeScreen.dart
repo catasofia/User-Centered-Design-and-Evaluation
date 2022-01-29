03eff088c6c10b566ec8e49324bab3b2cb6aba1a
patriciavilaoist <patriciavilao@tecnico.ulisboa.pt>
2022-01-29 12:10:09 +0000

    bug fix scroll neighbor

--- lib/neighbor/neighborHomeScreen.dart
+++ lib/neighbor/neighborHomeScreen.dart
@@ -253,142 +253,137 @@ class _NeighborHomeState extends State<NeighborHome> {
       backgroundColor: Colors.white,
       body: Padding(
           padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 30.0),
-          child: Center(
-            //child: contractLink.isLoading
-           // ? CircularProgressIndicator()
-           // :
-            child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon: const Icon(
-                        Icons.person_outline,
-                        color: Colors.black,
-                        size: 40.0,
+          child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(
+                      Icons.person_outline,
+                      color: Colors.black,
+                      size: 40.0,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => NeighborProfile()),
+                      );
+                    },
+                  ),
+                  SizedBox(width: 10.0),
+                  Container(
+                    width: 235.0,
+                    height: 42.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24.0),
+                      color: Colors.grey[200],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        SizedBox(width: 15.0),
+                        Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                        SizedBox(width: 10.0),
+                        Text(
+                          'Search',
+                          style: TextStyle(
+                            fontFamily: 'Arial',
+                            fontSize: 20,
+                            color: Colors.black,
+                            height: 1,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.notifications_active_outlined,
+                      color: Colors.black,
+                      size: 38.0,
+                    ),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) => _buildPopupNotification(context),
+                      );
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 20,),
+              Center(
+                child: Container(
+                  width: 220,
+                  height: 60,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Color(0xFF48ACBE)
                       ),
-                      onPressed: () {
+                      onPressed: (){
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => NeighborProfile()),
+                          MaterialPageRoute(builder: (context) => SuggestTask()),
                         );
                       },
-                    ),
-                    SizedBox(width: 10.0),
-                    Container(
-                      width: 235.0,
-                      height: 42.0,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(24.0),
-                        color: Colors.grey[200],
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          SizedBox(width: 15.0),
-                          Icon(
-                            Icons.search,
-                            color: Colors.black,
-                          ),
-                          SizedBox(width: 10.0),
-                          Text(
-                            'Search',
-                            style: TextStyle(
-                              fontFamily: 'Arial',
-                              fontSize: 20,
-                              color: Colors.black,
-                              height: 1,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: 8.0),
-                    IconButton(
-                      icon: const Icon(
-                        Icons.notifications_active_outlined,
-                        color: Colors.black,
-                        size: 38.0,
-                      ),
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) => _buildPopupNotification(context),
-                        );
-                      },
-                    ),
+                      child: Text('Suggest new task',
+                        style: TextStyle(color: Colors.black,
+                        letterSpacing: 1.0,
+                        fontSize: 20,),)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 0.0),
+                child: Text(
+                  'Tasks to Evaluate',
+                  style: TextStyle(
+                    color: Colors.black,
+                    letterSpacing: 2.0,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    for (var i in tasksToEvaluate) tasksCard(i),
                   ],
                 ),
-                SizedBox(height: 20,),
-                Center(
-                  child: Container(
-                    width: 220,
-                    height: 60,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Color(0xFF48ACBE)
-                        ),
-                        onPressed: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => SuggestTask()),
-                          );
-                        },
-                        child: Text('Suggest new task',
-                          style: TextStyle(color: Colors.black,
-                          letterSpacing: 1.0,
-                          fontSize: 20,),)),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 0.0),
+                child: Text(
+                  'Tasks Evaluated',
+                  style: TextStyle(
+                    color: Colors.black,
+                    letterSpacing: 2.0,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 0.0),
-                  child: Text(
-                    'Tasks to Evaluate',
-                    style: TextStyle(
-                      color: Colors.black,
-                      letterSpacing: 2.0,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    for (var i in tasksEvaluated) tasksCard(i),
+                  ],
                 ),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      for (var i in tasksToEvaluate) tasksCard(i),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 0.0),
-                  child: Text(
-                    'Tasks Evaluated',
-                    style: TextStyle(
-                      color: Colors.black,
-                      letterSpacing: 2.0,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      for (var i in tasksEvaluated) tasksCard(i),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ),
+          ),
       ),
     );
   }
